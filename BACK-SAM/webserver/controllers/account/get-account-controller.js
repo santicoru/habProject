8c6bbb7389be15d11e863daedf3f415dc1d60f0f
andycs22 <andycs2208@gmail.com>
2019-12-07 16:24:45 +0100

    organización en carpetas

---
+++ BACK-SAM/webserver/controllers/account/get-account-controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+async function getAccount(req, res, next){
+return res.status(200).send();
+}
+
+module.exports = getAccount;
---
+++ BACK-SAM/webserver/routes/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const account = require('./account-router');
+const auth = require('./auth-router');
+
+module.exports = {
+    account,
+    auth,
+};
