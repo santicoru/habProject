66468a7dc0b97b79f55dac3d777145bae6ed7b9d
andycs22 <andycs2208@gmail.com>
2019-12-11 22:24:52 +0100

    organización carpetas

--- SAM-PROJECT-BACKEND/webserver/controllers/account/get-account-controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-async function getAccount(req, res, next){
-return res.status(200).send();
-}
-
-module.exports = getAccount;
--- SAM-PROJECT-BACKEND/webserver/routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const account = require('./account-router');
-const auth = require('./auth-router');
-
-module.exports = {
-    account,
-    auth,
-};
