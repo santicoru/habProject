e9a00529b06f63dacc4670dae9ac929e597ddb97
andycs22 <andycs2208@gmail.com>
2019-12-26 14:39:11 +0100

    add en product GET-PRODUCT-CONTROLLER en back verificado en postman

--- react-sam/src/components/EditProduct.js
+++ react-sam/src/components/EditProduct.js
@@ -6,7 +6,7 @@ export function EditProduct() {
     <section className="editProduct">
       <h1> Editar produducto </h1>
       <button className="showAllForEdit">Mostrar todos los productos</button>
-      <label for="search">Buscar</label>
+      <label>Buscar</label>
       <input type="search" name="search" placeholder="Nombre del producto" />
       <button className="search">Buscar</button>
       <ul className="listProduct">
--- react-sam/src/http/ProductService.js
+++ react-sam/src/http/ProductService.js
@@ -1,13 +1,22 @@
 import axios from 'axios';
 
-export function createProductCo({ name, description, category, image, initPrice, discount, finalPrice }) {
+export function createProductCo({ name, description, category, file, init_price, discount, final_price }) {
   return axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/product`, {
-    name,
-    description,
-    category,
-    image,
-    initPrice,
-    discount,
-    finalPrice,
-  })
+    file,
+    datos: {
+      name,
+      description,
+      category,
+      init_price,
+      discount,
+      final_price
+    }
+  },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        "Accept": "application/json",
+        "type": "formData"
+      }
+    })
 }
